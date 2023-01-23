a5638eaa7596c6e860302050598dd58b0ec6f146
Your Name <you@example.com>
2023-01-23 20:57:18 +0100

    afegit arxius

---
+++ B/exercici2_B.py
@@ -0,0 +1,4 @@
+myTulpa = (1,2,"uxue",18,9,"uxue")
+print(len(myTulpa))
+print(18 in myTulpa)
+print(myTulpa.count("uxue"))
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import exercici1_A as ej1
-
+import exercici1_B as ej1B
 
 print(ej1.años(2004))
+print(ej1B.edat)
